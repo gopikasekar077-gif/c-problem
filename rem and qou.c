a1c185e7f36e5901a10a158c355f837d02b939be
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-10 10:53:51 +0530

    Add files via upload

---
+++ rem and qou.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int a=9;
+int b=2;
+printf("Quotient=%d remainder=%d\n",a/b,a%b);
+
+int c=10;
+https://www.onlinegdb.com/online_c_compiler#editor_1int d=3;
+printf("Quotient=%d remainder=%d\n",c/d,c%d);
+
+int e=20;
+int f=4;
+printf("Remainder=%d remainder=%d\n",e/f,e%f);
+    return 0;
