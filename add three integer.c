74204b7cc5a3ff5c161466fc8b7ee39206410d26
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-10 11:00:57 +0530

    Add files via upload

---
+++ add three integer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int a=2;
+int b=3;
+int c=5;
+printf("%d\n",a+b+c);
+
+int d=1;
+int e=4;
+int f=5;
+printf("%d\n",d+e+f);
+
+int g=3;
+int h=3;
+int i=4;
+printf("%d",g+h+i);
+    return 0;
+}
