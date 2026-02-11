a98d48e41cd8e29d57980650a012bc27344a6d90
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 14:00:55 +0530

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+ {
+    int num=18;
+    if(num>=18){
+        printf("Eligible to vote");
+    }
+    else{
+        printf("not eligible");
+    }
+    
+        
+    return 0;
+}
