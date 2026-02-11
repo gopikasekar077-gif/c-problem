e6edc4044728e2644beeb261675aebf1e09365a4
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 11:01:05 +0530

    Add files via upload

---
+++ positive,negative,zero.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+ {
+    int a=5;
+    if(a>0){
+        printf("positive number");}
+    else if(a<0){
+        printf("negative number");
+    }
+    else{
+        printf("zero");
+    }
+    return 0;
+}
