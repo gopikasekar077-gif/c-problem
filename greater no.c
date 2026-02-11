d100232944c8ce997d328bde71285e901fc2f425
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 11:15:42 +0530

    Add files via upload

---
+++ greater no.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+ {
+    int a=20;
+    int b=10;
+    if (a>b){
+         printf(" 20 is greater");
+    }
+    else{
+         printf("20 is lesser");
+    }
+        
+    return 0;
+}
