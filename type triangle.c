b20c94e0fd6f6f4b0dba7e92aaf7555144106606
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 14:55:45 +0530

    Add files via upload

---
+++ type triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+ {
+    int a=5,b=5,c=5;
+    if(a==b&&b==c){
+        printf("equilateral triangle");
+    }
+    else{
+        printf("not a triangle");
+    }
+    return 0;
+}
