a104a501db94dcfde8d8c987145e4f1485107f82
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 13:40:53 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+ {
+    char num='a';
+    if(num=='a'||num=='e'||num=='i'||num=='o'||num=='u')
+    {
+        printf("vowel");
+    }
+    else{
+        printf("constant");
+    }
+        
+    return 0;
+}
