1c82ddae4192c5048254344007fb6f9991767539
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 14:49:45 +0530

    Add files via upload

---
+++ div by11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+ {
+    int n=121;
+    if(n%11==0){
+        printf("divisible by 11");
+    }
+    else{
+        printf("not divisible");
+    }
+    return 0;
+}
