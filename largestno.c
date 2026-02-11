9a1ff19077fd36f749dc458e21178e31cd2010d6
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 14:11:41 +0530

    Add files via upload

---
+++ largestno.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+ {
+    int a=10;
+    int b=25;
+    int c=15;
+    if(a<b){
+        printf("25 is largest");
+    }
+    else if(b>c){
+        printf("10 lesser");
+    }
+    else{
+        printf("number");
+    }
+    
+    return 0;
+}
