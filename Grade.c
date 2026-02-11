41dfb0775b2e21b9e7ca462a14eb2711568150d2
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 14:21:50 +0530

    Add files via upload

---
+++ Grade.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+ {
+    int n=82;
+    if(n>=90){
+        printf("Grade A");
+    }
+    if(n>=75){
+        printf(" Grade B");
+    }
+    else{
+        printf("Grade c");
+    }
+    
+    return 0;
+}
