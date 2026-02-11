0516a936d37ac271cee0313b654d69ec7b0c97de
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 14:46:21 +0530

    Add files via upload

---
+++ uppercase.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+ {
+    char word='G';
+    if(word>='A'&&word<='Z'){
+        printf("uppercase letter");
+    }
+    else{
+        printf("lowercase");
+    }
+    
+    return 0;
+}
