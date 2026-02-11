192549906c4ad7e08b70465251771e75e4fa2585
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 14:05:45 +0530

    Add files via upload

---
+++ 3digitno.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+ {
+    int num=345;
+    if(num>=100&&num<=999){
+    printf("three digit number");
+    }
+    else{
+        printf("not digit number"); 
+    }
+        
+    return 0;
+}
