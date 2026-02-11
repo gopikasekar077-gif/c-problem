177ed62b05f3f65a4785e0535dbbab67ffbff9dd
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 11:19:21 +0530

    Add files via upload

---
+++ leap year.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+ {
+    int a=2024;
+    if (a %4==0){
+         printf(" leap year");
+    }
+    else{
+         printf("not a leap year");
+    }
+        
+    return 0;
+}
