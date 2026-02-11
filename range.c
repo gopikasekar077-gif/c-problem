34c575d8202437eb66ac4972bbe90e887268bc3a
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 14:59:48 +0530

    Add files via upload

---
+++ range.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+ {
+    int n=15;
+    if(n>=10&&n<=20){
+        printf("number is within range");
+    }
+    else{
+        printf("not a range");
+    }
+    return 0;
+}
