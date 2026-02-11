8c326e371c38010fd1d1cdb28d06d41df168ea04
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-11 14:54:05 +0530

    Add files via upload

---
+++ valid triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+ {
+    int a=60,b=60,c=60;
+    if(a==b&&b==c){
+        printf("valid triangle");
+    }
+    else{
+        printf("not a triangle");
+    }
+    return 0;
+}
