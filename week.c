7e54977e600d5fb62fac6e9e5a3bcc152cedf3a2
gopikasekar077-gif <gopikasekar077@gmail.com>
2026-02-12 09:38:51 +0530

    Add files via upload

---
+++ week.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+ {
+    int week=3;
+    switch(week){
+        case 1:
+        printf("monday");
+        break;
+        case 2:
+        printf("tuesday");
+        break;
+        case 3:
+        printf("wednesday");
+        break;
+        case 4:
+        printf("thursday");
+        break;
+        case 5:
+        printf("friday");
+        break;
+    }
+    return 0;
+}
